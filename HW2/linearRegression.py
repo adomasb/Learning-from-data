ae95342e6b91257d24d23fc48a583e7c910a95f4
adomasb <adomasbudrys@gmail.com>
2014-10-11 20:44:56 +0300

    rewritten

--- HW2/linearRegression.py
+++ HW2/linearRegression.py
@@ -9,82 +9,77 @@ import matplotlib.pyplot as plt
 
 # number of points
 N = 100
+# number of points for out sample error
 
-######## DATA & TARGET FUNCTION #####
+####### Functions #####
 
-# generates data
+
+# generates data frame of N points
 def data_set(N):
-    # data
-    data = pd.DataFrame({'x0': np.repeat(1, N),
+    return pd.DataFrame({'x0': np.repeat(1, N),
                          'x1': np.random.uniform(low=-1, high=1, size=N),
                          'x2': np.random.uniform(low=-1, high=1, size=N)})
 
-    # target function
+
+# creates target function and returns intercept & slope
+def target_function():
     point1 = np.random.uniform(low=-1, high=1, size=2)
     point2 = np.random.uniform(low=-1, high=1, size=2)
 
     a = (point1[1]-point2[1])/(point1[0]-point2[0])
     b = (point1[0]*point2[1]-point1[1]*point2[0])/(point1[0]-point2[0])
+    return a, b
 
-    # final data
-    sign = pd.DataFrame(np.sign(data['x2']-a*data['x1']-b))
+
+# assigns signs to data set by target function an returns updated data frame with y column
+def signs(data_set, target_function):
+    sign = pd.DataFrame(np.sign(data_set['x2']-target_function[0]*data_set['x1']-target_function[1]))
     sign.columns = ['y']
+    return pd.merge(data_set, sign, left_index=True, right_index=True)
 
-    return pd.merge(data, sign, left_index=True, right_index=True)
 
-# plotting data
-data = data_set(100)
-plt.xlim([-1, 1])
-plt.ylim([-1, 1])
-plt.scatter(x=data.x1, y=data.x2, c=np.where(data.y == 1, 'r', 'b'))
-plt.show()
-
-######## ALGORITHM IMPLEMENTATION ######
-
-# separating data
-data = data_set(100)
-X = data[['x0', 'x1', 'x2']].as_matrix()
-y = data['y'].as_matrix()
-
-# beta hat
-def beta_hat(X, y):
-    beta_hat = np.dot(np.dot(linalg.inv(np.dot(X.transpose(), X)), X.transpose()), y)
-    return beta_hat
-
-# points for plotting
-beta = beta_hat(X, y)
-points_beta = -beta[0]/beta[2] - beta[1]/beta[2] * np.array([1, -1])
-
-# plotting
-plt.xlim([-1, 1])
-plt.ylim([-1, 1])
-plt.plot([1, -1], points_beta, color='yellow')
-plt.scatter(x=data.x1, y=data.x2, c=np.where(data.y == 1, 'r', 'b'))
-plt.show()
-
-# calculates in sample error
-def E_in(data):
-    data = data_set(100)
+# calculates beta hat of regression
+def beta_hat(data):
     X = data[['x0', 'x1', 'x2']].as_matrix()
     y = data['y'].as_matrix()
-    beta = beta_hat(X, y)
+    return np.dot(np.dot(linalg.inv(np.dot(X.transpose(), X)), X.transpose()), y)
+
+
+# calculates in sample error
+def E_in(N):
+    # target function
+    f = target_function()
+    # generating data
+    data = signs(data_set(N), f)
+    # calculating beta
+    beta = beta_hat(data)
+    # subsetting data
     data_subset = data[['x0', 'x1', 'x2']].as_matrix()
-    return sum(data['y']!=np.sign((data_subset*beta).sum(axis=1)))/data.shape[0]
+    return sum(data['y'] != np.sign((data_subset*beta).sum(axis=1)))/data.shape[0]
+
+
+# calculates out sample error
+def E_out(N, n):
+    # target function
+    f = target_function()
+    # generating data
+    data = signs(data_set(N), f)
+    # calculating beta
+    beta = beta_hat(data)
+
+    # out of sample data
+    data_out = signs(data_set(n), f)
+    # subsetting data
+    data_out_subset = data_out[['x0', 'x1', 'x2']].as_matrix()
+    return sum(data_out['y'] != np.sign((data_out_subset*beta).sum(axis=1)))/data_out.shape[0]
+
 
 # 5 question answer
 e_ins = []
-[e_ins.append(E_in(data_set(100))) for i in np.arange(1000)]
+[e_ins.append(E_in(100)) for i in np.arange(1000)]
 print(np.mean(e_ins))
 
-#
-data = data_set(100)
-X = data[['x0', 'x1', 'x2']].as_matrix()
-y = data['y'].as_matrix()
-beta = beta_hat(X, y)
-
-
-
-
-
-
-
+# 6 question answer
+e_outs = []
+[e_outs.append(E_out(100, 1000)) for i in np.arange(1000)]
+print(np.mean(e_outs))
